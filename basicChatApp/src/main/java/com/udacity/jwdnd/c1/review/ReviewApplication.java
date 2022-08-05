03f312fa5db8480fb58172cf2507b0431e4f5978
Enhui Jiang <68130850+enhuijiang@users.noreply.github.com>
2022-08-05 12:51:51 -0400

    Add files via upload

---
+++ basicChatApp/src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.c1.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SpringBootApplication
+public class ReviewApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ReviewApplication.class, args);
+	}
+}
